1bb3c0df84be833d96bc0fc52d0daebc0403f040
Han Khuu <h.khuu01@gmail.com>
2018-11-01 20:47:05 -0400

    changed imports to exports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 @NgModule({
-  imports: [
-    CommonModule
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }

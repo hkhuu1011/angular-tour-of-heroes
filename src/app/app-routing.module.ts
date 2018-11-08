e0e8818a934b444b5502917c2d8d923231a6206a
Han Khuu <h.khuu01@gmail.com>
2018-11-07 20:13:23 -0500

    detail link working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
-	{ path: 'heroes', component: HeroesComponent },
-	{ path: 'dashboard', component: DashboardComponent },
-	{ path: 'detail/:id', component: HeroDetailComponent },
 	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+	{ path: 'dashboard', component: DashboardComponent },
+	{ path: 'heroes', component: HeroesComponent },
+	{ path: 'detail/:id', component: HeroDetailComponent }
 ];
 
 
